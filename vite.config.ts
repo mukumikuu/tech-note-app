316587dd254f4518228ddac474fc75a8f9995748
Unfortunated <thanyathornwiengperm@gmail.com>
2026-01-27 10:48:15 +0700

    setup: tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  plugins: [react(), tailwindcss()],
+  base: './',
   build: {
-    outDir: "dist-react",
+    outDir: 'dist-react',
   },
   server: {
     port: 5123,
