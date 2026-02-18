5560ef47f8ada8ec45a4de91494c7d2eb610cc67
Unfortunated <thanyathornwiengperm@gmail.com>
2026-02-18 22:17:55 +0700

    feat:extra highlight

--- src/frontend/features/syntaxhighlight/languagemap.ts
+++ src/frontend/features/syntaxhighlight/languagemap.ts
@@ -1,11 +1,15 @@
 import { javascript } from '@codemirror/lang-javascript'
 import { python } from '@codemirror/lang-python'
+import { cpp } from '@codemirror/lang-cpp'
+import { java } from '@codemirror/lang-java'
 import { LanguageSupport, StreamLanguage } from '@codemirror/language'
 import { shell } from '@codemirror/legacy-modes/mode/shell'
 import type { Language } from '../../../shared/language'
 
 type LanguageMap = Record<Language, LanguageSupport | StreamLanguage<Language>>
 const languageMap: LanguageMap = {
+  Cpp: cpp(),
+  Java: java(),
   JavaScript: javascript(),
   Python: python(),
   Shell: StreamLanguage.define(shell),
