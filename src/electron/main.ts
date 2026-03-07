d92744eab88863980d9c74c8445f29621775bb1e
Unfortunated <thanyathornwiengperm@gmail.com>
2026-03-07 12:53:39 +0700

    feat: improvement

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,31 +1,40 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import serverStart from './backend/server.js'
-import { isDev } from './util.js'
+import { isDev, logAppMemory, logMemory } from './util.js'
 
 let mainWindow: BrowserWindow | null = null
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      devTools: true,
     },
   })
 
+  setInterval(() => {
+    logMemory()
+    logAppMemory(app)
+  }, 2500)
+
   if (isDev()) {
     mainWindow.loadURL('http://localhost:5123')
+    import('react-devtools-electron')
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
   }
 
   const { httpServer, kernel, PORT } = serverStart()
   kernel.start()
+  logMemory()
   httpServer.listen(PORT, () => {
     console.log(`Electron Socket.IO server running on ${PORT}`)
   })
 
   app.on('window-all-closed', () => {
+    logMemory()
     httpServer.close()
     kernel.stop()
     app.quit()
--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,3 +1,28 @@
 export function isDev() {
-  return process.env.NODE_ENV === "development"; // Check if the app is running in development mode
+  return process.env.NODE_ENV === 'development' // Check if the app is running in development mode
+}
+
+export function logMemory() {
+  const mem = process.memoryUsage()
+  const heap = process.getHeapStatistics()
+  console.log(`RSS: ${(mem.rss / 1024 / 1024).toFixed(2)} MB`)
+  console.log(`heapUsed: ${(mem.heapUsed / 1024 / 1024).toFixed(2)} MB`)
+  console.log(`heapTotal: ${(mem.heapTotal / 1024 / 1024).toFixed(2)} MB`)
+  console.log(`mallocedMemory: ${(heap.mallocedMemory / 1024).toFixed(2)} MB`)
+  console.log(`peakMemory: ${(heap.peakMallocedMemory / 1024).toFixed(2)} MB`)
+}
+
+export function logAppMemory(app: Electron.App) {
+  const metrics = app.getAppMetrics()
+  metrics.forEach((metric) => {
+    const cpu = metric.cpu.percentCPUUsage
+    const memoryKB = metric.memory.workingSetSize
+    const memoryMB = memoryKB / 1024
+    console.log({
+      pid: metric.pid,
+      type: metric.type,
+      cpuPercent: cpu,
+      memoryMB: memoryMB.toFixed(2),
+    })
+  })
 }
