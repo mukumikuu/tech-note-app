9117d5c8f862aa71df123394ae3b1b55e28f6ca5
Unfortunated <thanyathornwiengperm@gmail.com>
2026-03-25 15:08:11 +0700

    fix: test to correspond with codeblock.tsx

--- src/test/integration/frontend/syntaxhighlighting.test.tsx
+++ src/test/integration/frontend/syntaxhighlighting.test.tsx
@@ -22,15 +22,15 @@ describe('syntaxhighlighting', () => {
     const onRemove = jest.fn()
     const onAdd = jest.fn()
     const onExecute = jest.fn()
-    const onValueChange = jest.fn()
+    const onContentChange = jest.fn()
     const onLangChange = jest.fn()
     render(
       <CodeBlock
         id='a'
         blockIndex={0}
-        value='Write something...'
+        content='Write something...'
         language='JavaScript'
-        onValueChange={onValueChange}
+        onContentChange={onContentChange}
         onLangChange={onLangChange}
         onAdd={onAdd}
         onRemove={onRemove}
